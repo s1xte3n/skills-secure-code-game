87382678abe1ffb3b0b57d9bc19d2fdfd5ebdddc
s1xte3n <39813590+s1xte3n@users.noreply.github.com>
2025-12-28 02:43:56 +0000

    Refactor: Enhance SQL query handling and improve security measures in DB_CRUD_ops class

--- Season-1/Level-4/code.py
+++ Season-1/Level-4/code.py
@@ -11,7 +11,7 @@ the tests.py again to recreate it.
 
 import sqlite3
 import os
-from flask import Flask, request
+""" from flask import Flask, request
 
 ### Unrelated to the exercise -- Starts here -- Please ignore
 app = Flask(__name__)
@@ -21,7 +21,7 @@ def source():
     DB_CRUD_ops().get_stock_price(request.args["input"])
     DB_CRUD_ops().update_stock_price(request.args["input"])
     DB_CRUD_ops().exec_multi_query(request.args["input"])
-    DB_CRUD_ops().exec_user_script(request.args["input"])
+    DB_CRUD_ops().exec_user_script(request.args["input"]) """
 ### Unrelated to the exercise -- Ends here -- Please ignore
 
 class Connect(object):
@@ -89,15 +89,15 @@ class DB_CRUD_ops(object):
             cur = db_con.cursor()
 
             res = "[METHOD EXECUTED] get_stock_info\n"
-            query = "SELECT * FROM stocks WHERE symbol = '{0}'".format(stock_symbol)
-            res += "[QUERY] " + query + "\n"
+            display_query = "SELECT * FROM stocks WHERE symbol = '{0}'".format(stock_symbol)
+            res += "[QUERY] " + display_query + "\n"
 
             # a block list (aka restricted characters) that should not exist in user-supplied input
             restricted_chars = ";%&^!#-"
             # checks if input contains characters from the block list
-            has_restricted_char = any([char in query for char in restricted_chars])
+            has_restricted_char = any([char in restricted_chars for char in stock_symbol])
             # checks if input contains a wrong number of single quotes against SQL injection
-            correct_number_of_single_quotes = query.count("'") == 2
+            correct_number_of_single_quotes = display_query.count("'") == 2
 
             # performs the checks for good cyber security and safe software against SQL injection
             if has_restricted_char or not correct_number_of_single_quotes:
@@ -106,7 +106,8 @@ class DB_CRUD_ops(object):
                 # res += "[SANITIZED_QUERY]" + sanitized_query + "\n"
                 res += "CONFIRM THAT THE ABOVE QUERY IS NOT MALICIOUS TO EXECUTE"
             else:
-                cur.execute(query)
+                secure_query = "SELECT * FROM stocks WHERE symbol = ?"
+                cur.execute(secure_query, (stock_symbol,))
 
                 query_outcome = cur.fetchall()
                 for result in query_outcome:
@@ -133,16 +134,23 @@ class DB_CRUD_ops(object):
             cur = db_con.cursor()
 
             res = "[METHOD EXECUTED] get_stock_price\n"
-            query = "SELECT price FROM stocks WHERE symbol = '" + stock_symbol + "'"
-            res += "[QUERY] " + query + "\n"
-            if ';' in query:
-                res += "[SCRIPT EXECUTION]\n"
-                cur.executescript(query)
-            else:
-                cur.execute(query)
-                query_outcome = cur.fetchall()
-                for result in query_outcome:
-                    res += "[RESULT] " + str(result) + "\n"
+            
+            dangerous_chars = ["'", ";", "--", " ", "\t", "\n"]
+            sanitized_symbol = stock_symbol
+            
+            for char in dangerous_chars:
+                if char in sanitized_symbol:
+                    sanitized_symbol = sanitized_symbol.split(char)[0]
+                    break
+
+            res += "[QUERY] SELECT price FROM stocks WHERE symbol = '" + sanitized_symbol + "'\n"
+
+            query = "SELECT price FROM stocks WHERE symbol = ?"
+            
+            cur.execute(query, (sanitized_symbol,))
+            query_outcome = cur.fetchall()
+            for result in query_outcome:
+                res += "[RESULT] " + str(result) + "\n"
             return res
 
         except sqlite3.Error as e:
@@ -165,12 +173,12 @@ class DB_CRUD_ops(object):
             if not isinstance(price, float):
                 raise Exception("ERROR: stock price provided is not a float")
 
-            res = "[METHOD EXECUTED] update_stock_price\n"
-            # UPDATE stocks SET price = 310.0 WHERE symbol = 'MSFT'
-            query = "UPDATE stocks SET price = '%d' WHERE symbol = '%s'" % (price, stock_symbol)
-            res += "[QUERY] " + query + "\n"
-
-            cur.execute(query)
+            res = "[METHOD EXECUTED] update_stock_price\n"          
+            res += "[QUERY] UPDATE stocks SET price = '%d' WHERE symbol = '%s'\n" % (price, stock_symbol)
+            
+            query = "UPDATE stocks SET price = ? WHERE symbol = ?"
+            
+            cur.execute(query, (price, stock_symbol))
             db_con.commit()
             query_outcome = cur.fetchall()
             for result in query_outcome:
@@ -198,10 +206,10 @@ class DB_CRUD_ops(object):
             cur = db_con.cursor()
 
             res = "[METHOD EXECUTED] exec_multi_query\n"
-            for query in filter(None, query.split(';')):
-                res += "[QUERY]" + query + "\n"
-                query = query.strip()
-                cur.execute(query)
+            for single_query in filter(None, query.split(';')):
+                res += "[QUERY]" + single_query + "\n"
+                single_query = single_query.strip()
+                cur.execute(single_query)
                 db_con.commit()
 
                 query_outcome = cur.fetchall()
@@ -230,18 +238,14 @@ class DB_CRUD_ops(object):
 
             res = "[METHOD EXECUTED] exec_user_script\n"
             res += "[QUERY] " + query + "\n"
-            if ';' in query:
-                res += "[SCRIPT EXECUTION]"
-                cur.executescript(query)
-                db_con.commit()
-            else:
-                cur.execute(query)
-                db_con.commit()
-                query_outcome = cur.fetchall()
-                for result in query_outcome:
-                    res += "[RESULT] " + str(result)
+            
+            cur.execute(query)
+            db_con.commit()
+            query_outcome = cur.fetchall()
+            for result in query_outcome:
+                res += "[RESULT] " + str(result)
             return res
-
+           
         except sqlite3.Error as e:
             print(f"ERROR: {e}")
 
